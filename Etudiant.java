4c9745f76630469dc7bce6caa2139a593c36736b
abdelilah <abdelilahkouzih2003@gmail.com>
2024-03-30 13:18:01 +0000

    etudiant.java modifier par develop !

--- Etudiant.java
+++ Etudiant.java
@@ -2,19 +2,21 @@ public class Etudiant {
     private String nom;
     private int age;
     private String niveau;
+    private String filiere;
 
-    public Etudiant(String nom, int age, String niveau) {
+    public Etudiant(String nom, int age, String niveau, String filiere) {
         this.nom = nom;
         this.age = age;
         this.niveau = niveau;
+	this.filiere = filiere
     }
 
     public void afficherInfos() {
-        System.out.println("Nom: " + nom + ", Age: " + age + ", Niveau: " + niveau);
+        System.out.println("Nom: " + nom + ", Age: " + age + ", Niveau: " + niveau + ", filière:" + filiere);
     }
 
     public static void main(String[] args) {
-        Etudiant etudiant1 = new Etudiant("Jean", 20, "Licence");
+        Etudiant etudiant1 = new Etudiant("Jean", 20, "Licence" , "GIGD");
         etudiant1.afficherInfos();
         etudiant1.setAge(21);
         etudiant1.afficherInfos();
